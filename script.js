1a2deca4ccbfb2ba7088e6978ecdd9ecb0da4ade
Lavansi98 <sriansika3@gmail.com>
2025-01-26 22:03:41 +0530

    Changed codes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('submit').addEventListener('click', () => {
       const name = document.getElementById('username').value.trim(); // Get the input value and trim extra spaces
-      document.getElementById('getname').textContent = name ? `Hello, ${name} your age is :` : 'Please enter your name.';
+      document.getElementById('getname').textContent = `Hello, ${name}`;
       document.getElementById('username').value = ''; //clear the input field
     })
 
@@ -12,11 +12,7 @@ document.querySelector('#submit').addEventListener('click',()=>{
         today = new Date(),
         timenow = today.getTime(),
         value = timenow - time,
-        sec = Math.floor(value/1000),
-        min = Math.floor(value/(1000*60)),
-        hour = Math.floor(value/(1000*60*60)),
         days = Math.floor(value/(1000*60*60*24)),
-        week = Math.floor(value/(1000*60*60*24*7)),
         month = Math.floor(value/(1000*60*60*24*30.4375)),
         year = Math.floor(value/(1000*60*60*24*365.25));
 
@@ -24,13 +20,9 @@ document.querySelector('#submit').addEventListener('click',()=>{
             document.querySelector(x).innerHTML = y;
         }
         
-        insert('#year',year + ' Years');
-        insert('#month',month + ' months');
-        insert('#week',week + ' weeks');
-        insert('#day',days + ' days');
-        insert('#hour',hour + ' hours');
-        insert('#min',min + ' minutes');
-        insert('#sec',sec + ' seconds');
+
+
+        insert('#year', `you are ${year} years, ${month} months and ${days} days old.`);
         
     }
     setInterval(update,1000)
